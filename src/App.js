ce3bac0046d8c35ec70497e7e716ce115a2e8860
pzync <shkhrgrv@gmail.com>
2019-07-12 16:59:51 +0530

    Background Image effects like Blur on Drag

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { motion } from "framer-motion";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
       <motion.div
@@ -11,12 +13,15 @@ function App() {
         drag
         dragElastic={1}
         dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        onDragStart={() => setValue(20)}
+        onDragEnd={() => setValue(0)}
         animate={{
           scale: [1, 0.6, 1.2, 1],
           borderRadius: [`15%`, `50%`, `50%`, `15%`],
           rotate: [0, 180, -180, 0]
         }}
       />
+      <div className="App-bg" style={{ filter: `blur(${value}px)` }} />
     </div>
   );
 }
